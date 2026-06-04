780729d0feb56b9d871af1fef773bc05bc9e3c0f
Perla <perla.pbs1326@gmail.com>
2026-06-03 22:57:24 -0300

    Corrige conflitos de merge

--- abb.py
+++ abb.py
@@ -3,13 +3,7 @@ class NoABB:
     def __init__(self, paciente):
 
         self.paciente = paciente
-<<<<<<< HEAD
         self.esquerda = None
-=======
-
-        self.esquerda = None
-
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
         self.direita = None
 
 
@@ -18,9 +12,7 @@ class ABB:
     def __init__(self):
 
         self.raiz = None
-<<<<<<< HEAD
         self.comparacoes = 0
-
 
     # =========================
     # INSERÇÃO
@@ -28,25 +20,10 @@ class ABB:
     def inserir(self, paciente):
 
         self.raiz = self._inserir(self.raiz, paciente)
-=======
-
-        self.comparacoes = 0
-
-
-    def inserir(self, paciente):
-
-        self.raiz = self._inserir(
-
-            self.raiz,
-            paciente
-        )
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
-
 
     def _inserir(self, no, paciente):
 
         if no is None:
-<<<<<<< HEAD
             return NoABB(paciente)
 
         self.comparacoes += 1
@@ -58,42 +35,15 @@ class ABB:
         else:
 
             no.direita = self._inserir(no.direita, paciente)
-=======
-
-            return NoABB(paciente)
-
-
-        self.comparacoes += 1
-
-
-        if paciente.score < no.paciente.score:
-
-            no.esquerda = self._inserir(
-
-                no.esquerda,
-                paciente
-            )
-
-        else:
-
-            no.direita = self._inserir(
-
-                no.direita,
-                paciente
-            )
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
 
         return no
 
-
-<<<<<<< HEAD
     # =========================
     # BUSCA POR ID DA IMAGEM
     # =========================
     def buscar(self, nome_imagem):
 
         return self._buscar(self.raiz, nome_imagem)
-
 
     def _buscar(self, no, nome_imagem):
 
@@ -104,39 +54,25 @@ class ABB:
             return no.paciente
 
         esquerda = self._buscar(no.esquerda, nome_imagem)
+
         if esquerda:
             return esquerda
 
         return self._buscar(no.direita, nome_imagem)
 
-
     # =========================
     # ALTURA
     # =========================
-=======
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
     def altura(self):
 
         return self._altura(self.raiz)
 
-
     def _altura(self, no):
 
         if no is None:
-<<<<<<< HEAD
             return 0
 
         return max(
             self._altura(no.esquerda),
             self._altura(no.direita)
         ) + 1
-=======
-
-            return 0
-
-        esquerda = self._altura(no.esquerda)
-
-        direita = self._altura(no.direita)
-
-        return max(esquerda, direita) + 1
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
--- heap_maxima.py
+++ heap_maxima.py
@@ -3,9 +3,7 @@ class HeapMaxima:
     def __init__(self):
 
         self.heap = []
-<<<<<<< HEAD
         self.comparacoes = 0
-
 
     # =========================
     # INSERÇÃO
@@ -14,7 +12,6 @@ class HeapMaxima:
 
         self.heap.append(paciente)
         self._subir(len(self.heap) - 1)
-
 
     # =========================
     # SUBIR (HEAPIFY UP)
@@ -27,18 +24,20 @@ class HeapMaxima:
 
             self.comparacoes += 1
 
-            # Heap Máxima: maior score fica em cima
             if self.heap[indice].score > self.heap[pai].score:
 
-                self.heap[indice], self.heap[pai] = self.heap[pai], self.heap[indice]
+                self.heap[indice], self.heap[pai] = (
+                    self.heap[pai],
+                    self.heap[indice]
+                )
+
                 indice = pai
 
             else:
                 break
 
-
     # =========================
-    # REMOÇÃO (caso precise no futuro)
+    # REMOÇÃO
     # =========================
     def remover_max(self):
 
@@ -55,118 +54,47 @@ class HeapMaxima:
 
         return raiz
 
-
     # =========================
     # DESCER (HEAPIFY DOWN)
     # =========================
     def _descer(self, indice):
 
         n = len(self.heap)
-=======
-
-        self.comparacoes = 0
-
-
-    def inserir(self, paciente):
-
-        self.heap.append(paciente)
-
-        self.subir(len(self.heap) - 1)
-
-
-    def subir(self, indice):
-
-        pai = (indice - 1) // 2
-
-        while indice > 0 and self.heap[indice].score > self.heap[pai].score:
-
-            self.comparacoes += 1
-
-            self.heap[indice], self.heap[pai] = (
-
-                self.heap[pai],
-                self.heap[indice]
-            )
-
-            indice = pai
-
-            pai = (indice - 1) // 2
-
-
-    def remover_maior(self):
-
-        if len(self.heap) == 0:
-
-            return None
-
-        maior = self.heap[0]
-
-        ultimo = self.heap.pop()
-
-        if self.heap:
-
-            self.heap[0] = ultimo
-
-            self.descer(0)
-
-        return maior
-
-
-    def descer(self, indice):
-
-        tamanho = len(self.heap)
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
 
         while True:
 
             maior = indice
-<<<<<<< HEAD
             esquerda = 2 * indice + 1
             direita = 2 * indice + 2
 
             if esquerda < n:
-=======
-
-            esquerda = 2 * indice + 1
-
-            direita = 2 * indice + 2
-
-
-            if esquerda < tamanho:
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
 
                 self.comparacoes += 1
 
                 if self.heap[esquerda].score > self.heap[maior].score:
-<<<<<<< HEAD
                     maior = esquerda
 
             if direita < n:
-=======
-
-                    maior = esquerda
-
-
-            if direita < tamanho:
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
 
                 self.comparacoes += 1
 
                 if self.heap[direita].score > self.heap[maior].score:
-<<<<<<< HEAD
                     maior = direita
 
             if maior != indice:
 
-                self.heap[indice], self.heap[maior] = self.heap[maior], self.heap[indice]
+                self.heap[indice], self.heap[maior] = (
+                    self.heap[maior],
+                    self.heap[indice]
+                )
+
                 indice = maior
 
             else:
                 break
 
-
     # =========================
-    # ALTURA (opcional para relatório)
+    # ALTURA
     # =========================
     def altura(self):
 
@@ -176,21 +104,3 @@ class HeapMaxima:
             return 0
 
         return math.floor(math.log2(len(self.heap))) + 1
-=======
-
-                    maior = direita
-
-
-            if maior == indice:
-
-                break
-
-
-            self.heap[indice], self.heap[maior] = (
-
-                self.heap[maior],
-                self.heap[indice]
-            )
-
-            indice = maior
->>>>>>> e20cc8ef63140f68e9561497dad77d3bd2329e2f
